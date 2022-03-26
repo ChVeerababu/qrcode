e4ffc489e0264c86a5f0d4e41bda9316170a0ffb
Veerababu <veerababuch1@gmail.com>
2022-03-26 12:00:08 +0530

    dfgh

--- app.py
+++ app.py
@@ -51,4 +51,5 @@ app=Flask(__name__)
 def data():
     return get_data_browsers('http://qrcode.samisme.cf:8080/services/example')
 
-app.run(host='0.0.0.0',port=8080)
+if __name__ == '__main__':
+    app.run()
